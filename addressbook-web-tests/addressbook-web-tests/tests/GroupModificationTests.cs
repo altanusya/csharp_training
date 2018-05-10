1e7a71ea693650efd0377bf2a41fb1251b0ada7f
Altana Tsedenova <altana.zedenova@viacode.com>
2018-05-10 14:20:44 +0300

    test 2 pull request

--- addressbook-web-tests/addressbook-web-tests/tests/GroupModificationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupModificationTests.cs
@@ -21,6 +21,7 @@ namespace WebAddressdookTests
 
 
             List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData oldData = oldGroups[0];
 
             app.Groups.Modify(0, newData);
 
@@ -32,6 +33,14 @@ namespace WebAddressdookTests
             oldGroups.Sort();
             newGroups.Sort();
             Assert.AreEqual(oldGroups, newGroups);
+
+            foreach(GroupData group in newGroups)
+            {
+                if(group.Id == oldData.Id)
+                {
+                    Assert.AreEqual(newData.Name, group.Name);
+                }
+            }
         }
 
     }
